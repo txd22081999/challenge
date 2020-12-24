5128e2811d6b7cd58fa9726e0b6e37585128bd30
txd22081999 <txd22081999@gmail.com>
2020-12-25 02:29:47 +0700

    add base64 endpoint

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
-const http = require('http')
+const http = require('http');
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  if (req.url === '/') return respondHello(req, res)
+  if (req.url === '/') return respondHello(req, res);
 
-  res.end()
-})
+  res.end();
+});
 
-function respondHello (req, res) {
-  res.end(JSON.stringify({ msg: 'hello' }))
+function respondHello(req, res) {
+  res.end(JSON.stringify({ msg: 'hello' }));
 }
 
-server.listen(PORT)
-console.log(`Server listening on port ${PORT}`)
+server.listen(PORT);
+console.log(`Server listening on port ${PORT}`);
 
-if (require.main !== module) module.exports = server
+if (require.main !== module) module.exports = server;
+
+// Implement base64 function
